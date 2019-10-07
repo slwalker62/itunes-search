43f1c2010dddc16478e6ee7dc807b4d0a9d2d2b3
Sean Walker <s.walker2362@gmail.com>
2019-10-06 21:41:09 -0500

    cerntered and styled

--- src/app/itunes.service.ts
+++ src/app/itunes.service.ts
@@ -14,18 +14,6 @@ export class ItunesService {
   constructor(private http: HttpClient) { }
 
   getSongs(searchInput: string) {
-
-        
-    let headers = new HttpHeaders();
-    let params = new HttpParams();
-    params = params.append('term', searchInput);
-    params = params.append('media', 'music');
-    params = params.append('limit', '3');
-    params = params.append('callback', 'CALLBACK_JSON');
-
-    //let x = this.http.get(this.searchUrl, {params: params}).subscribe(res=> { console.log(res);});
-
-//.map(item => {return new SearchResult})
    return this.http.jsonp(`${this.searchUrl}?term=${searchInput}&media=music&limit=3`, 'callback');
   }
 }
--- src/app/search-box/search-box.component.ts
+++ src/app/search-box/search-box.component.ts
@@ -7,36 +7,48 @@ import { ItunesService } from '../itunes.service';
   templateUrl: './search-box.component.html',
   styleUrls: ['./search-box.component.css']
 })
+
 export class SearchBoxComponent implements OnInit {
   // Input field in search box
   searchInput: string;
   // Member for search reslt bindings
   searchResults: SearchResult[];
+  // columns to display 
   displayedColumns: string[] = ['name', 'artist', 'album'];
+
+  searchClicked: boolean;
 
   constructor(private itunesService: ItunesService) { }
 
   ngOnInit() {
-
+    let searchClicked = false;
   }
 
   // Search event on button click
   onSearch() {
-    // Call itunes service with search input
-    this.itunesService.getSongs(this.searchInput)
-    .subscribe(
-      res => {
-        this.searchResults = res['results'].map(
-          item => {
-            return new SearchResult(
-                          item.trackName, 
-                          item.artistName, 
-                          item.collectionName
-                        )
-          }
-        )        
-      }
-    );
+    if(!this.searchClicked){
+      if(this.searchInput != null) {
+        console.log(this.searchInput);
+        // Call itunes service with search input
+        this.itunesService.getSongs(this.searchInput)
+          .subscribe(
+            res => {
+              this.searchResults = res['results'].map(
+                item => {
+                  return new SearchResult(
+                                item.trackName, 
+                                item.artistName, 
+                                item.collectionName
+                              )
+                }
+              )        
+            }
+          );
+      }  
+    } else {
+      this.searchResults = null;
+    }
+    this.searchClicked = !this.searchClicked;
   }
 
 }
